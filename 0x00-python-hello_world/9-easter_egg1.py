4a2b2e49932bbda3702f65ad91ad16f8f4b412ff
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-06 14:57:06 +0000

    Task 5, 6, 7, 8, 9

---
+++ 0x00-python-hello_world/9-easter_egg1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+str= "The Zen of Python\n\nBeautiful is better than ugly.\n\
+Explicit is better than implicit. Simple is better than complex.\n\
+Complex is better than complicated Flat is better than nested.\n\
+Sparse is better than dense. Readability counts.\n\
+Special cases aren't special enough to break the rules.\n\
+Although practicality beats purity. Errors should never pass silently.\n\
+Unless explicitly silenced. In the face of ambiguity, refuse the temptation to guess.\n\
+There should be one -- and preferably only one -- obvious way to do it.\n\
+Although that way may not be obvious at first unless you're Dutch.\n\
+Now is better than never. Although never is often better than *right* now.\n\
+If the implementation is hard to explain, it's a bad idea.:\n\
+If the implementation is easy to explain, it may be a good idea.\n\
+Namespaces are one honking great idea -- let's do more of those!"
+print(str)
