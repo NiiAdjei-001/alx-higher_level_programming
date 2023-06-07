4cc615eca1ff26660173ec953a2fc812598bacc6
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-07 05:57:45 +0000

    fixed negative power error

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,10 +2,10 @@
 def pow(a, b):
     res = 1
     for i in range(0, abs(b)):
-        if b > 0:
-            res *= a
-        elif b == 0:
+        if b == 0:
             pass
         else:
-            res *= 1/a
+            res *= a
+    if b < 0:
+        return 1/res
     return res
