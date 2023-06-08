8b405cb6ec0fc9cfae00ea4628f978570c54b8b3
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-08 13:55:33 +0000

    Creating project 0x02-python-import_modules

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = 0
-    while i < len(str):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
+    l = len(str)
+    for i in range(0, l):
+        ci = ord(str[i]) # character's ascii number
+        c = chr(ci - 32) # character offset by -32
+        if ci >= 97 and ci <= 122:
+            print("{}".format( (c + "\n") if (i == (l - 1)) else c), end="")
         else:
-            print("{}".format(str[i]), end="")
+            print("{}".format( (str[i] + "\n") if (i == (l - 1)) else str[i]) , end="")
         i += 1
-    print()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ALX Higher Level Programming Section
 Learning python programming with ALX
+
+Folder List
+- 0x00. Python - Hello, World
+- 0x01. Python - if/else, loops, functions
+- 0x02. Python - import & modules
