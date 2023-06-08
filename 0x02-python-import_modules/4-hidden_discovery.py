e17eac3e8bb1e6cee8799d6acf765b1c2bc58dc8
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-08 22:38:18 +0000

    Completed Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import fnmatch
+    from fnmatch import fnmatch
     pyc_path = "hidden_4.pyc"
     attribute_list = dir(pyc_path);
     for element in attribute_list:
-        if not fnmatch.fnmatch(element,'__*'):
+        if not fnmatch(element,'__*'):
             print(element)
