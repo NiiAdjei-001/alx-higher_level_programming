da6f0e03622591c2f3c583caaa244a5cec7a5609
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-13 02:51:06 +0000

    Task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replaces an element in a particular index of a list
+
+        Args:
+        my_list: list of integers
+        idx: index of element
+        element: new element to replace existing index value
+
+        Returns:
+        Returns unaugmented list if idx is negative
+        or greater than list size
+        else returns augmented list
+    """
+    if idx < 0 or idx >= len(my_list):
+        my_list
+    else:
+        my_list[idx] = element
+        return my_list
