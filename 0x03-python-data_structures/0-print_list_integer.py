b57fcd0c393eae15bc66fe5c1dbeddd642087b17
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-13 02:21:06 +0000

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    size = len(my_list)
-    for i in range(0, size):
+    """Print s a list of integer values
+
+        Args:
+        my_list: list of integers
+
+        Returns:
+        Does not return anything
+    """
+    for i in range(0, len(my_list)):
         print("{}".format(my_list[i]))
