f82f53b23f2880b563f6004be6b8ec8002c3b852
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-15 02:34:59 +0000

    Changed Task 0 implementation

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -8,13 +8,11 @@ def square_matrix_simple(matrix=[]):
         Returns:
         Returns a 2 dimensional matrix with squared values
      """
-    r_len = len(matrix)
-    squares = []
-    for row in range(0, r_len):
-        c_len = len(matrix[row])
-        temp = []
-        for col in range(0, c_len):
-            temp.append(matrix[row][col] ** 2)
-        squares.append(temp.copy())
-        temp.clear()
-    return squares
+    new_matrix = []
+    for row in matrix:
+        tmp = []
+        for col in row:
+            tmp.append(col * col)
+        new_matrix.append(tmp.copy())
+        tmp.clear()
+    return new_matrix
