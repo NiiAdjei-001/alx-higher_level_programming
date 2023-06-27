799b2c0be0b10c61261c3f4b853cb81d192198fc
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-27 05:03:24 +0000

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """My Safe Print Integer List
+
+    Args:
+        value: value to be printed
+
+    Returns:
+        The returns True if element is an Integer else False
+    """
+    state = False
+    try: 
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
