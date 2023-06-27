2b78e8efa929d77cc530c8208fd5edc6fa7d272c
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-27 04:48:25 +0000

    Initilized Project 0x05 Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """My Safe Print List Function
+
+    Args:
+        my_list: list that can contain any type of data
+        x: number of elements to print
+
+    Returns:
+        The returns a list of x elements
+    """
+    ite = 0
+    try: 
+        for ite in range(x):
+            print("{}".format(my_list[ite]), end="")
+        print("{}".format(""))
+    except IndexError:
+        print("{}".format(""))
+    return ite;
