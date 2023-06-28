b4efe4239f6cbb30a8cd3786c859df4a8b0f6b3d
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-28 22:22:39 +0000

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
+"""This module defines a square class
+"""
+
+
 class Square:
+    """Square class
+        Used to create a square object
+    """
     def __init__(self, size=0):
+        """__init__ method
+            Initializes the square object
+
+            Args:
+                size: length of the square
+        """
         try:
             self.__size = size
+            if type(self.__size) != int:
+                raise TypeError
+            if self.__size < 0:
+                raise ValueError
         except TypeError:
-            print("size must be an integer\n")
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
