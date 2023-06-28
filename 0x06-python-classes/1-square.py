9f0ece7bca47af57121de95aa661c817dffe628f
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-28 22:05:47 +0000

    Fixed documentation bug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""This module defines a square class
+"""
+
+
 class Square:
+    """Square class
+        Used to create a square object
+    """
     def __init__(self, size):
+        """__init__ method
+            Initializes the square object
+
+            Args:
+                size: length of the square
+        """
         self.__size = size
