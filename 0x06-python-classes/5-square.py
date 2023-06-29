714940d534d8e8c15c842f404ca1384a7c4f238a
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-29 21:04:50 +0000

    Completed Task 5, 6

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This module defines a square class
+"""
+
+
+class Square:
+    """Square class
+        Used to create a square object
+    """
+    def __init__(self, size=0):
+        """__init__ method
+            Initializes the square object
+
+            Args:
+                size: length of the square
+        """
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square
+
+            Returns:
+                size * size
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Returns the size of the square
+
+            Returns:
+                size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square
+
+            Args:
+                value: new value of size
+        """
+        if not (type(value) == int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(0, self.__size):
+                for col in range(0, self.__size):
+                    print("#", end="")
+                print()
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+"""This module defines a square class
+"""
+__type_err_mes_size = "size must be an integer"
+__value_err_mes_size = "size must be >= 0"
+__type_err_mes_position = "position must be a tuple of 2 positive integers"
+
+
+class Square:
+    """Square class
+        Used to create a square object
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """__init__ method
+            Initializes the square object
+
+            Args:
+                size: length of the square
+        """
+        if not (type(size) == int):
+            raise TypeError(__type_err_mes_size)
+        if size < 0:
+            raise ValueError(__value_err_mes_size)
+        if not ((type(position[0]) == int) and (type(position[1]) == int)):
+            raise TypeError(__type_err_mes_position)
+        if ((position[0] < 0) or (position[1] < 0)):
+            raise TypeError(__type_err_mes_position)
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Returns the area of the square
+
+            Returns:
+                size * size
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Gets the size of the square
+
+            Returns:
+               size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Sets the size of the square
+
+            Args:
+                size: x unit
+        """
+        if not (type(size) == int):
+            raise TypeError(__type_err_mes_size)
+        if size < 0:
+            raise ValueError(__value_err_mes_size)
+        self.__size = size
+
+    @property
+    def position(self):
+        """Gets the position coordinates of the square
+
+            Returns:
+                position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        """Sets the position coordinates of the square
+
+            Args:
+                position: position coordinates (x, y)
+        """
+        if not ((type(position[0]) == int) and (type(position[1]) == int)):
+            raise TypeError(__type_err_mes_position)
+        if ((position[0] <= 0) or (postion[1] < 0)):
+            raise TypeError(__type_err_mes_position)
+        self.__position = position
+
+    def area(self):
+        """Returns the area of the square
+
+        Returns:
+            size * size
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints a square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            # print("\n" * self.position[1], end="")
+            for row in range(0, self.__size):
+                for col in range(0, self.__size + self.__position[0]):
+                    if col < self.__position[0]:
+                        print(" ", end="")
+                    else:
+                        print("#", end="")
+                print()
