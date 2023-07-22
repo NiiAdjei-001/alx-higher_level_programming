b0e7a479bc5cc0aa99f75949ec4939617474e762
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-07-22 18:48:15 +0000

    Task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Say my name Module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Say my name function
+
+        Args
+            first_name: first name string
+            last_name: last name string
+
+        Return:
+            returns full name
+    """
+    if not (type(first_name) == str):
+        raise TypeError("first_name must be a string")
+    if not (type(last_name) == str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
