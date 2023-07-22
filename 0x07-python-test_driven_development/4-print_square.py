b0b1aa6198251d16dbfe754af5ce8e8dcfc659e2
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-07-22 15:37:41 +0000

    Task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Print square module
+"""
+
+
+def print_square(size):
+    """Print square function
+
+        Args:
+        size: length of the square
+    """
+    if not type(size) in [int]:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for r in range(size):
+        for c in range(size):
+            print("#", end="")
+        print("", end="") if r == (size - 1) else print("")
