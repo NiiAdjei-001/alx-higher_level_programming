d337bf8ae26fcee5975bd9b4d9356c332a21f52c
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-06 04:00:12 +0000

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""LockedClass Module
+"""
+
+
+class LockedClass:
+    """LockedClass Class"""
+    
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """__init__ method
+            Initializes object
+        """
+        pass
