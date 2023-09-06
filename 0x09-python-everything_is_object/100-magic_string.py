96c15c62a7fd0a9e27ea7f21436749e619247d6b
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-06 02:48:42 +0000

    Fixed style error on 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,11 +1,4 @@
-#!/usr/bin/python3
-"""Magic String Function Module"""
-
-
-ls = []
-
-
+ls=[]
 def magic_string():
-    """Magic String Function"""
     ls.append('BestSchool') if len(ls) == 0 else ls.append(', BestSchool')
     return ''.join(ls)
