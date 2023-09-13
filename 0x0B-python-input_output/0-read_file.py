77ec43b585d669ec2f2889b06407b57d67cfe907
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-13 03:11:30 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,14 +5,9 @@
 
 def read_file(filename=""):
     """Read File and print to stdout
-    
+    """
     try:
         with open(filename, 'r', encoding='utf-8') as file_reader:
             print(file_reader.read())
-    except Exception:
-        pass
-    finally:
-        file_reader.close()
-    """
-    with open(filename, 'r', encoding='utf-8') as file_reader:
-        print(file_reader.read())
+    except Exception as error:
+        print(error)
