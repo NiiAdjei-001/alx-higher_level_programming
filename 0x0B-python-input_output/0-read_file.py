21f8b67e57c302d7fc4bd8bc5eed7f7c9547b218
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-13 02:53:33 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Read File Module
 """
-"""
+
+
 def read_file(filename=""):
+    """Read File and print to stdout
+    """
     with open(filename, 'r', encoding='utf-8') as _file:
         print(_file.read())
