01bf2a08ce3705e4e7737da883094be7e1879dda
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-10-20 05:59:12 +0000

    Task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Display records from second_table where name exists
-SELECT score, name FROM second_table WHERE name
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
