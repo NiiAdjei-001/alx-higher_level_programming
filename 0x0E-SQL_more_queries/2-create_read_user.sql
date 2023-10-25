6c6a7e58911abbe7df7c90564423423ee3d0c70a
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-10-25 05:43:03 +0000

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creates a user 'user_0d_1@localhost' with pass 'user_0d_1_pwd'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT PRIVILEGES ON hbtn_0d_2.* 'user_0d_1'@'localhost';
+GRANT SELECT PRIVILEGES ON hbtn_0d_2.* 'user_0d_2'@'localhost';
