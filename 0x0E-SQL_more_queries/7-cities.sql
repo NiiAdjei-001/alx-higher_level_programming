794b1887a82799aa184f397c23d10c4b05534327
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-10-27 15:20:28 +0000

    Add 7-cites.sql file

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create database hbtn_0d_usa.
+-- Create table cities in hbtn_0d_usa.
+-- Cities attributes (id, name)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
