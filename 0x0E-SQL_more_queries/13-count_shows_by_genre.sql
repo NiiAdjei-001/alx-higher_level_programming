05eea7f1e203e6034ca200bafbc516563deeac66
Draco <niiadjeiosae.technology@gmail.com>
2023-11-12 23:00:39 +0000

    13-count_shows_by_genre.sql created

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- Show a list of genres and number of shows under such genres.
+SELECT 
+    tv_genres.name as 'genre', 
+    COUNT(tv_genres.name) as 'number_of_shows'
+FROM 
+    tv_genres
+INNER JOIN 
+    tv_show_genres 
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY
+    tv_genres.name
+ORDER BY COUNT(tv_genres.name) DESC;
