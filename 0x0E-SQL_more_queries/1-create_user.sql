e185c36a7599ea2a43fe0019e75fabc1f5cfc4a2
Draco <niiadjeiosae.technology@gmail.com>
2023-11-12 22:15:41 +0000

    1-create_user.sql corrected

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,13 @@
 -- Creates a user 'user_0d_1@localhost' with pass 'user_0d_1_pwd'
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * 'user_0d_1'@'localhost';
+-- Grant user privileges and permissions
+--  Privileges :: ALL PRIVILEGES, CREATE, DROP, DELETE, INSERT, SELECT, UPDATE
+--  Permissions :: <DATABASE_NAME>.<TABLE_NAME>
+-- Flush privileges to refresh
+CREATE USER [IF NOT EXISTS] 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES 
+ON *.* 
+TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
