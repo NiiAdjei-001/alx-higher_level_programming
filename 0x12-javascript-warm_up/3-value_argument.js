7d1c5fa3dcebd31b7d71959c283759ee4dfe120f
Draco <niiadjeiosae.technology@gmail.com>
2023-11-13 17:55:07 +0000

    2-arguments.js, 3-value_argument.js created

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+let count = 0;
+argv.forEach(()=> {
+  count += 1;
+})
+if (count <= 2) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
