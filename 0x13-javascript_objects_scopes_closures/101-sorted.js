d8367e9c7f8256b7ad7067c82386fa85f408bfeb
Draco <niiadjeiosae.technology@gmail.com>
2023-11-15 22:20:40 +0000

    101-sorted.js fixed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const sortedDict = {};
-for (let [key, value] of Object.entries(dict)) {
+for (const [key, value] of Object.entries(dict)) {
   if (sortedDict[value.toString()] === undefined) {
     sortedDict[value.toString()] = [];
     sortedDict[value.toString()].push(key.toString());
