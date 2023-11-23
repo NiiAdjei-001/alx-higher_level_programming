69ae269c5837b120b7fe44b29664e47173ebc942
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-11-23 16:58:23 +0000

    augment credentials for connectivity on 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,26 +3,22 @@
 Select States Module
 """
 import MySQLdb
+import sys
 
 
-DB_DETAIL = {
-    'host': "localhost",
-    'port': 3306,
-    'user': "beta_usr",
-    'passwd': "beta_pwd",
-    'db': "hbtn_0e_0_usa"
-}
-ALX_DB_DETAIL = {
-    'host': "localhost",
-    'port': 3306,
-    'user': "root",
-    'passwd': "root",
-    'db': "hbtn_0e_0_usa"
-}
-db = MySQLdb.connect(**ALX_DB_DETAIL)
-cursor = db.cursor()
-query = "SELECT id, name FROM states;"
-cursor.execute(query)
-records = cursor.fetchall()
-for record in records:
-    print(record)
+argv = sys.argv
+if argv.__len__() == 4:
+    ALX_DB_DETAIL = {
+        'host': "localhost",
+        'port': 3306,
+        'user': argv[1],
+        'passwd': argv[2],
+        'db': argv[3]
+    }
+    db = MySQLdb.connect(**ALX_DB_DETAIL)
+    cursor = db.cursor()
+    query = "SELECT id, name FROM states;"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
