cbac3dc61c0d8ccdfd09e57e247166b0b541f72f
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-11-23 16:49:18 +0000

    README.md 0-select_states.py created

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Select States Module
+"""
+import MySQLdb
+
+
+DB_DETAIL = {
+    'host': "localhost",
+    'port': 3306,
+    'user': "beta_usr",
+    'passwd': "beta_pwd",
+    'db': "hbtn_0e_0_usa"
+}
+ALX_DB_DETAIL = {
+    'host': "localhost",
+    'port': 3306,
+    'user': "root",
+    'passwd': "root",
+    'db': "hbtn_0e_0_usa"
+}
+db =  MySQLdb.connect(**DB_DETAIL)
+cursor = db.cursor()
+query = "SELECT id, name FROM states;"
+cursor.execute(query)
+records = cursor.fetchall()
+for record in records:
+    print(record)
