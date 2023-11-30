fb280bec524c00de2b40550ccc56b779dd4f97b1
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-11-30 04:18:38 +0000

    task 7 modified

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """ Model State Fetch All Module
 """
-import MySQLdb
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
 from model_state import Base, State
 
 
 argv = sys.argv
 if argv.__len__() == 4:
-    ALX_DB_DETAIL = {
-        'host': "localhost",
-        'port': 3306,
-        'user': argv[1],
-        'passwd': argv[2],
-        'db': argv[3]
-    }
-    db = MySQLdb.connect(**ALX_DB_DETAIL)
-    cursor = db.cursor()
-    query = """SELECT id, name
-            FROM states
-            ORDER BY states.id ASC;"""
-    cursor.execute(query)
-    records = cursor.fetchall()
-    my_list = [x[0] for x in records]
+    url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
+                                          argv[2],
+                                          'localhost',
+                                          '3306',
+                                          argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    
+    records = session.query(State)
     for record in records:
-        print("{}: {}".format(record[0], record[1]))
-    cursor.close()
-    db.close()
+        print("{}: {}".format(record.id, record.name))
+    session.close()
