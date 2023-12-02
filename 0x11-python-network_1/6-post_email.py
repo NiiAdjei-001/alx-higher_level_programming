a0f4ead8ec75a200078311716473b1177c768382
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-12-02 05:55:57 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to post an email
+    argv[1] - url
+    argv[2] - email
+"""
+from urllib import parse, request
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    if argv.__len__() >= 3:
+        url = argv[1]
+        data = {
+            'email': argv[2]
+        }
+        res = requests.post(url,data=data)
+        print(res.text)
