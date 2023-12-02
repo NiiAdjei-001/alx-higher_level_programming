a169d981157c9359074af499d040fc35f8c37637
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2023-12-02 05:47:49 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script to display X-Request-Id of an http response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+
+    if argv.__len__() >= 2:
+        res = requests.get(argv[1])
+        print('{}'.format(res.headers['X-Request-Id']))
