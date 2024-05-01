5d80c22afbbbff2e9f239d76671a5ccea0b6627b
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-01 18:47:40 +0000

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Model State Fetch All States Contain 'a'
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+argv = sys.argv
+if __name__ == "__main__":
+    if argv.__len__() == 5:
+        url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
+                                              argv[2],
+                                              'localhost',
+                                              '3306',
+                                              argv[3])
+        engine = create_engine(url)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+        record = session.query(State) \
+            .filter(State.name == argv[4]) \
+            .first()
+        if record:
+            print("{}".format(record.id))
+        else:
+            print("Not found")
+        session.close()
