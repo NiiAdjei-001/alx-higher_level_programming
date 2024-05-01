e9e2f9b91b33f88739a0e5a97dd1ff0f2abc9d41
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-01 20:43:58 +0000

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Model State Fetch All States Contain 'a'
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
@@ -9,7 +9,7 @@ import sys
 
 argv = sys.argv
 if __name__ == "__main__":
-    if argv.__len__() == 5:
+    if argv.__len__() >= 5:
         url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
                                               argv[2],
                                               'localhost',
@@ -20,11 +20,20 @@ if __name__ == "__main__":
         Session.configure(bind=engine)
         session = Session()
 
-        record = session.query(State) \
-            .filter(State.name == argv[4]) \
-            .first()
+        #record = session.query(State) \
+        #    .filter(State.name == argv[4]) \
+        #    .first()
+        #if record:
+        #    print("{}".format(record.id))
+        #else:
+        #    print("Not found")
+        #session.close()
+
+        stmt = select(State.id).where(State.name == argv[4])
+        record = session.execute(stmt).first()
         if record:
             print("{}".format(record.id))
         else:
             print("Not found")
         session.close()
+        
