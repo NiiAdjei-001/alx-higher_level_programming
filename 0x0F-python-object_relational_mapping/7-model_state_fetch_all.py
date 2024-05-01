8cb2343613d305656567abccdfcbbcd35146cedb
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-01 13:46:19 +0000

    Python Object Relational Mapping Task 07

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,18 +8,19 @@ import sys
 
 
 argv = sys.argv
-if argv.__len__() == 4:
-    url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
-                                          argv[2],
-                                          'localhost',
-                                          '3306',
-                                          argv[3])
-    engine = create_engine(url)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-    
-    records = session.query(State)
-    for record in records:
-        print("{}: {}".format(record.id, record.name))
-    session.close()
+if __name__ == "__main__":
+    if argv.__len__() == 4:
+        url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
+                                              argv[2],
+                                              'localhost',
+                                              '3306',
+                                              argv[3])
+        engine = create_engine(url)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+        records = session.query(State)
+        for record in records:
+            print("{}: {}".format(record.id, record.name))
+        session.close()
