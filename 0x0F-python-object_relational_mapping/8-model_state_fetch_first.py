9c7c5632135f045a8e906ed7740c87f1f22faaea
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-01 14:07:35 +0000

    Python Object Relational Mapping Task 08

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Model State Fetch First State
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+argv = sys.argv
+if __name__ == "__main__":
+    if argv.__len__() == 4:
+        url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
+                                              argv[2],
+                                              'localhost',
+                                              '3306',
+                                              argv[3])
+        engine = create_engine(url)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+        record = session.query(State).first()
+        if record:
+            print("{}: {}".format(record.id, record.name))
+        else:
+            print("Nothing")
+        session.close()
