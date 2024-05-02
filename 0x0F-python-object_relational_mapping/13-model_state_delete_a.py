cd6280b51afd4384753ec09199780bc91d2231d5
Nii Adjei Osae <niiadjeiosae.technology@gmail.com>
2024-05-02 00:01:03 +0000

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Model State - Delete States Contain 'a'
+"""
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+argv = sys.argv
+if __name__ == "__main__":
+    if argv.__len__() >= 4:
+        url = 'mysql://{}:{}@{}:{}/{}'.format(argv[1],
+                                              argv[2],
+                                              'localhost',
+                                              '3306',
+                                              argv[3])
+        engine = create_engine(url)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+        states = session.query(State) \
+            .filter(State.name.ilike('%a%'))
+        session.delete(states)
+        session.commit()
+        session.close()
